78954e3d15698ef68fd0a48e88f2c0e8dde8434f
TMoiseyenko06 <tmoiseyenko06@gmail.com>
2024-08-26 16:09:38 -0400

    Initial Commit

---
+++ data_types.js
@@ -0,0 +1,6 @@
+studentName = "Alice";
+studentAge = 15;
+studentGrade = 10;
+isPresent = true;
+
+console.log(studentName,studentAge,studentGrade,isPresent)
---
+++ exploring_operators.js
@@ -0,0 +1,8 @@
+num1 = 1;
+num2 = 2;
+
+console.log("Sum:",num1 + num2);
+console.log("Difference:",num1 - num2);
+console.log("Product:",num1 * num2);
+console.log("Quotient:",num1/num2)
+
